6e15267441f63f98aba3ed15aaef3e839a88f5bc
oswaldo <vosdigital2021@gmail.com>
2022-08-03 16:25:58 -0500

    make it better

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from distutils.command.upload import upload
+from email.mime import image
+from turtle import title
+from xml.dom.minidom import CharacterData
+from django.db import models
+from django.forms import CharField
+import datetime
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to= 'blog/images')
+    date = models.DateField(datetime.date.today)
